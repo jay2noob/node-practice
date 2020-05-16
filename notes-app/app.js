fff2dfe0773460c4eed75a8f614933ea6280105f
John Fields <jjfields24@outlook.com>
2020-05-15 22:41:22 -0400

    used yargs package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,47 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes");
 
-const msg = getNotes();
-console.log(msg);
+// Customize yargs
+yargs.version("1.1.0");
 
-const greenMsg = chalk.green.inverse.bold("Success!");
-console.log(greenMsg);
+// Add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note!");
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List notes",
+  handler: function () {
+    console.log("Listing all notes!");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note!");
+  },
+});
+
+// Add, remove, read, list
+
+console.log(process.argv);
+console.log(yargs.argv);
