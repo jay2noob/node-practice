edc1abdc96329c07e068a2fc2cc2d72b0c550b99
John Fields <jjfields24@outlook.com>
2020-05-19 00:39:55 -0400

    change json file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,23 @@
-const book = {
-  title: "Ego is the enemy",
-  author: "Ryan Holiday",
-};
+const fs = require("fs");
+//const book = {
+//  title: "Ego is the enemy",
+//  author: "Ryan Holiday",
+//};
 
-const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+//const bookJSON = JSON.stringify(book);
+//fs.writeFileSync("1-json.json", bookJSON);
 
-const parsedData = JSON.parse(bookJSON);
-console.log(parsedData.author);
+//const dataBuffer = fs.readFileSync("1-json.json");
+//const dataJSON = dataBuffer.toString();
+//const data = JSON.parse(dataJSON);
+//console.log(data.title);
+
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const user = JSON.parse(dataJSON);
+
+user.name = "John";
+user.age = 39;
+
+const userJSON = JSON.stringify(user);
+fs.writeFileSync("1-json.json", userJSON);
