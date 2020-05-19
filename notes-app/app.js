2ba64c9adf69bf17bb29fb88f2bce7f230a4561c
John Fields <jjfields24@outlook.com>
2020-05-18 23:54:12 -0400

    added builder to add command

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,8 +9,21 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: function () {
-    console.log("Adding a new note!");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: ", argv.title);
+    console.log("Body: ", argv.body);
   },
 });
 
@@ -43,5 +56,5 @@ yargs.command({
 
 // Add, remove, read, list
 
-console.log(process.argv);
-console.log(yargs.argv);
+//console.log(process.argv);
+yargs.parse();
