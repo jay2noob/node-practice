4cbfb91b8c909345fc20eb1d274efb4ebe7db8e6
John Fields <jjfields24@outlook.com>
2020-05-22 00:17:02 -0400

    added saveNote and addNote functions

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -4,7 +4,28 @@ const getNotes = function () {
   return "Your notes...";
 };
 
-const addNote = function (title, body) {};
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const dupliateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+
+  if (dupliateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note title taken!");
+  }
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
 
 const loadNotes = function () {
   try {
