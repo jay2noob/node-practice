005a068ebc8a2ec136dc025c9de0ba3052cd5943
John Fields <jjfields24@outlook.com>
2020-05-21 23:56:49 -0400

    added addNote and loadNotes function

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,22 @@
+const fs = require("fs");
+
 const getNotes = function () {
   return "Your notes...";
 };
 
-module.exports = getNotes;
+const addNote = function (title, body) {};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (err) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
