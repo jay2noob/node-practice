2fca525ae8119e1d8e7b65e1a2cea5cecd8ca92d
John Fields <jjfields24@outlook.com>
2020-06-23 20:03:45 -0400

    converted to es6 arrow functions

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -5,7 +5,7 @@ const getNotes = function () {
   return "Your notes...";
 };
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
   const dupliateNotes = notes.filter(function (note) {
     return note.title === title;
@@ -23,7 +23,7 @@ const addNote = function (title, body) {
   }
 };
 
-const removeNote = function (title) {
+const removeNote = (title) => {
   const notes = loadNotes();
   const notesToKeep = notes.filter(function (note) {
     return note.title !== title;
@@ -36,12 +36,12 @@ const removeNote = function (title) {
   }
 };
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
 };
 
-const loadNotes = function () {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync("notes.json");
     const dataJSON = dataBuffer.toString();
